b85b587b2eed11a3a5fcd7b0049ad68fba49fb7d
robertphanbuh6 <robertphanbuh6@gmail.com>
2026-06-19 16:42:01 +0530

    server.js

--- server.js
+++ server.js
@@ -84,8 +84,8 @@ io.on("connection", (socket) => {
         console.log("DISCONNECTED:", socket.id);
     });
 });
-
+const PORT = process.env.PORT || 3000;
 /* ================= START SERVER ================= */
-server.listen(3000, () => {
-    console.log("SERVER RUNNING ON 3000");
+server.listen(PORT, () => {
+    console.log("SERVER RUNNING ON", PORT);
 });
